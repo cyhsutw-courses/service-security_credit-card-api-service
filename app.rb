26d5befa680d3f3ec374b208d62c9b40d8552652
Jevon Mckenzie <jevonmckenzie.wesleycollege@gmail.com>
2015-04-15 19:39:48 +0800

    Modified implementation of app.rb

--- app.rb
+++ app.rb
@@ -7,23 +7,20 @@ class CreditCardAPI < Sinatra::Base
   # TODO: add API paths
 
   get '/' do
-    'Credit Card app is up and running, API available at /api/v1/'
-  end
-
-  get '/api/v1/' do
-    'Services offered include <br>'\
-    'GET /api/v1/credit_card/validate?number=[your credit card number]<br>' \
-    'POST /api/v1/validate_checksum(numeric parameters: max, body)'
+    'Credit Card Application is up and running: API available at /api/v1/'
   end
 
   get '/api/v1/credit_card/validate' do
-    number = params[:number]
+    number = params[:card_number]
     halt 400 unless number
-
-    { hash: number.hash,
-    notes: 'Non-cryptographic hash,not for secure use'
+    card = CreditCard.new(number,nil,nil,nil)
+    {
+      card: number,
+      validated: card.validate_checksum
     }.to_json
+
   end
+
 
 
 
