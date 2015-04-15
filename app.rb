eac6f09a81813e627f5bdeaa3d835422e4563e18
Cheng-Yu Hsu <crazyming411@gmail.com>
2015-04-15 22:59:44 +0800

    Refactor `app.rb`.

--- app.rb
+++ app.rb
@@ -3,9 +3,8 @@ require 'sinatra'
 
 require_relative 'lib/credit_card.rb'
 
+# credit card api service
 class CreditCardAPI < Sinatra::Base
-  # TODO: add API paths
-
   get '/' do
     'Credit Card Application is up and running: API available at /api/v1/'
   end
@@ -13,15 +12,10 @@ class CreditCardAPI < Sinatra::Base
   get '/api/v1/credit_card/validate' do
     number = params[:card_number]
     halt 400 unless number
-    card = CreditCard.new(number,nil,nil,nil)
+    card = CreditCard.new(number, nil, nil, nil)
     {
       card: number,
       validated: card.validate_checksum
     }.to_json
-
   end
-
-
-
-
 end
