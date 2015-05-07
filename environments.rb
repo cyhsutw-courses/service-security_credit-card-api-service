7781fb0603d61c6ecc713c888cd61ab7995733fb
Amon <amonmoce@yahoo.fr>
2015-05-07 23:33:58 +0800

    Writing the tests for post request not yet completed

--- environments.rb
+++ environments.rb
@@ -2,3 +2,8 @@ configure :development do
   set :database, 'sqlite3:db/dev.db'
   set :show_exception, true
 end
+
+configure :test do
+  set :database, 'sqlite3:db/test.db'
+  set :show_exception, true
+end
--- spec/rack_test_helper.rb
+++ spec/rack_test_helper.rb
@@ -2,3 +2,10 @@ ENV['RACK_ENV'] = 'test'
 
 require 'minitest/autorun'
 require 'rack/test'
+require_relative '../app'
+
+include Rack::Test::Methods
+
+def app
+  CreditCardAPI
+end
