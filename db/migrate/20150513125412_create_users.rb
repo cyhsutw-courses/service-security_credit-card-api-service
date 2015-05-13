8a3ca3d1e1bb22a9faf39fffc2407a9efbbdcc5e
Cheng-Yu Hsu <crazyming411@gmail.com>
2015-05-13 20:59:18 +0800

    Migration file updated: now creates user.

--- db/migrate/20150513125412_create_users.rb
+++ db/migrate/20150513125412_create_users.rb
@@ -1,4 +1,15 @@
+# create user table
 class CreateUsers < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :username
+      # password related
+      t.string :hashed_password
+      t.string :password_salt
+      t.string :email
+      t.string :fullname
+      t.text :address
+      t.date :dob
+    end
   end
 end
