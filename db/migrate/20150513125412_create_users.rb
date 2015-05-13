4fee1a2938e3eb475f8652cbc9f3145008fc5e57
Cheng-Yu Hsu <crazyming411@gmail.com>
2015-05-13 21:02:24 +0800

    Update migration file: add database constraints.

--- db/migrate/20150513125412_create_users.rb
+++ db/migrate/20150513125412_create_users.rb
@@ -2,14 +2,14 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :username, null: false, unique: true
       # password related
-      t.string :hashed_password
-      t.string :password_salt
-      t.string :email
-      t.string :fullname
-      t.text :address
-      t.date :dob
+      t.string :hashed_password, null: false
+      t.string :password_salt, null: false
+      t.string :email, null: false
+      t.string :fullname, null: false
+      t.text :address, null: false
+      t.date :dob, null: false
     end
   end
 end
